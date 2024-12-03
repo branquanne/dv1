79e9617bd6f4eb88ed833008efb962d9fbbfc4a7
Bran Quanne <branmq@gmail.com>
2024-12-03 20:59:13 +0100

    added docs

--- OU4/queue.h
+++ OU4/queue.h
@@ -2,25 +2,104 @@
 #define QUEUE_H
 
 #include <stdbool.h>
+/**
+ * @defgroup queue_h QUEUE_H
+ *
+ * @brief The Queue module is a simple queue implementation.
+ *
+ * The queue is a data structure that allows elements to be inserted and removed in a first-in-first-out
+ * (FIFO) order. This means that the first element that was inserted into the queue
+ * is the first one to be removed.
+ *
+ * @author Bran Mjöberg Quanne
+ * @since 2024-12-03
+ * @{
+ */
 
-// The type for the queue.
+/**
+ * @brief The type for a queue.
+ *
+ * The type for a queue.
+ *
+ * @note The implementation of the queue is hidden from the user of the module and has to be
+ * defined in the implementation file.
+ */
 typedef struct queue Queue;
 
-// Create and return an empty queue.
+/**
+ * @brief Create an empty queue.
+ *
+ * This function creates a new queue and allocates memory for it.
+ *
+ * @param None.
+ *
+ * @return A pointer to the newly created queue.
+ *
+ * @note The caller is responsible for deallocating the memory used by the queue. This can be done using queue_destroy().
+ * @note The function does not check if the memory allocation is successful and thus it may result in an undefined behaviour.
+ */
 Queue *queue_create(void);
 
-// Destroy the queue.
+/**
+ * @brief Destroy the queue.
+ *
+ * This function deallocates the memory used by the queue.
+ *
+ * @param q A pointer to the queue to be destroyed.
+ *
+ * @return None.
+ *
+ * @note The function does not check if the queue exists and thus it may result in an undefined behaviour.
+ */
 void queue_destroy(Queue *q);
 
-// Add a value to the tail of the queue.
-// The value is copied to dynamically allocated memory.
+/**
+ * @brief Add a value to the tail of the queue.
+ *
+ * This function adds a value to the tail of the queue. The value is copied to
+ * dynamically allocated memory.
+ *
+ * @param q A pointer to the queue.
+ * @param value The value to be added to the queue.
+ *
+ * @return None.
+ *
+ * @note This function does not check if the memory allocation is successful and
+ * thus it may result in an undefined behaviour.
+ *
+ */
 void queue_enqueue(Queue *q, const char *value);
 
-// Remove the value at the head of the queue.
-// The caller is responsible for deallocating the returned pointer.
+/**
+ * @brief Remove the value at the head of the queue.
+ *
+ * This function removes the value at the head of the queue and returns a
+ * pointer to the value. The caller is responsible for deallocating the returned
+ * pointer.
+ *
+ * @param q A pointer to the queue.
+ *
+ * @return A pointer to the value at the head of the queue.
+ *
+ * @note This function does not check if the queue is empty and thus it may result
+ * in an undefined behaviour.
+ */
 char *queue_dequeue(Queue *q);
 
-// Check if the queue is empty.
+/**
+ * @brief Check if the queue is empty.
+ *
+ * This function checks if the queue is empty.
+ *
+ * @param q A pointer to the queue.
+ *
+ * @return True if the queue is empty, otherwise false.
+ *
+ * @note This function does not check if the queue exists and thus it may result
+ * in an undefined behaviour.
+ */
 bool queue_is_empty(const Queue *q);
-
+/**
+ * @}
+ */
 #endif /* QUEUE_H */
--- OU4/stack.h
+++ OU4/stack.h
@@ -4,7 +4,9 @@
 #include <stdbool.h>
 
 /**
- * @defgroup stack stack
+ * @defgroup stack_h STACK_H
+ *
+ * @brief The Stack module is a simple stack implementation.
  *
  * A stack is a data structure that allows elements to be inserted and removed
  * in a last-in-first-out (LIFO) order. This means that the last element that
@@ -15,22 +17,85 @@
  * @{
  */
 
-// The type for a stack.
+/**
+ * @brief The type for a stack.
+ *
+ * The type for a stack.
+ *
+ * @note The implementation of the stack is hidden from the user of the module and has to be
+ * defined in the implementation file.
+ */
 typedef struct stack Stack;
 
-// Create an empty stack.
+/**
+ * @brief Create an empty stack.
+ *
+ * This function creates a new stack and allocates memory for it.
+ *
+ * @param None.
+ *
+ * @return A pointer to the newly created stack.
+ *
+ * @note The caller is responsible for deallocating the memory used by the stack. This can be done using stack_destroy().
+ * @note The function does not check if the memory allocation is successful and thus it may result in an undefined behaviour.
+ */
 Stack *stack_create(void);
 
-// Destroy the stack.
+/**
+ * @brief Destroy the stack.
+ *
+ * This function deallocates the memory used by the stack.
+ *
+ * @param s A pointer to the stack to be destroyed.
+ *
+ * @return None.
+ *
+ * @note The function does not check if the stack exists and thus it may result in an undefined behaviour.
+ */
 void stack_destroy(Stack *s);
 
-// Push a value onto the stack.
+/**
+ * @brief Push a value onto the stack.
+ *
+ * This function pushes a value onto the stack.
+ *
+ * @param s A pointer to the stack.
+ * @param value The value to be pushed onto the stack.
+ *
+ * @return None.
+ *
+ * @note The function does not check if the stack exists and thus it may result in an undefined behaviour.
+ * @note The function does not check if the memory reallocation is successful and thus it may result in an undefined behaviour.
+ * @note The function does not check if the value is a valid number and thus it may result in an undefined behaviour.
+ *
+ */
 void stack_push(Stack *s, double value);
 
-// Pop the value at the top of the stack.
+/**
+ * @brief Pop the value at the top of the stack.
+ *
+ * This function pops the value at the top of the stack and returns it.
+ *
+ * @param s A pointer to the stack.
+ *
+ * @return The value at the top of the stack.
+ *
+ * @note The function does not check if the stack exists and thus it may result in an undefined behaviour.
+ * @note The function does not check if the stack is empty and thus it may result in an undefined behaviour.
+ */
 double stack_pop(Stack *s);
 
-// Check if the stack is empty.
+/**
+ * @brief Check if the stack is empty.
+ *
+ * This function checks if the stack is empty.
+ *
+ * @param s A pointer to the stack.
+ *
+ * @return True if the stack is empty, otherwise false.
+ *
+ * @note The function does not check if the stack exists and thus it may result in an undefined behaviour.
+ */
 bool stack_is_empty(const Stack *s);
 
 #endif /* STACK_H */
