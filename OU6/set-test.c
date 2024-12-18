835b1da12ecd1fb43825e309f527a4b392bddbc4
Bran Quanne <branmq@gmail.com>
2024-12-18 12:25:29 +0100

    changed everything

--- OU6/set-test.c
+++ OU6/set-test.c
@@ -3,21 +3,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-bool check_set_empty();
-bool check_set_single();
-bool check_set_insert();
-bool check_set_union();
-bool check_set_intersection();
-bool check_set_difference();
-bool check_set_get_values();
-bool check_set_member_of();
-bool check_set_is_empty();
-bool check_set_choose();
-bool check_set_equal();
-bool check_set_subset();
-bool check_set_remove();
-bool check_set_destroy();
-bool check_set_size();
+void check_set_empty(set *s1, set *s2);
+set *check_set_single(void);
+set *check_set_insert(void);
+void check_set_union(set *s1, set *s2);
+void check_set_intersection(set *s1, set *s2);
+void check_set_difference(set *s1, set *s2);
+void check_set_get_values(set *s);
+void check_set_member_of(set *s);
+void check_set_is_empty(set *s);
+void check_set_choose(set *s);
+void check_set_equal(set *s1, set *s2);
+void check_set_subset(set *s1, set *s2);
+void check_set_remove(set *s);
+// void check_set_destroy(set *s);
+void check_set_size(set *s);
 
 struct set {
     int capacity;
@@ -27,247 +27,267 @@ struct set {
 
 int main(void)
 {
-    printf("The functioning of set_single() has been tested ... \t\t%s\n", check_set_single() ? "OK" : "FAIL");
-    printf("The functioning of set_insert() has been tested ... \t\t%s\n", check_set_insert() ? "OK" : "FAIL");
-    printf("The functioning of set_union() has been tested ... \t\t%s\n", check_set_union() ? "OK" : "FAIL");
-    printf("The functioning of set_intersection() has been tested ... \t%s\n", check_set_intersection() ? "OK" : "FAIL");
-    printf("The functioning of set_difference() has been tested ... \t%s\n", check_set_difference() ? "OK" : "FAIL");
-    printf("The functioning of set_get_values() has been tested ... \t%s\n", check_set_get_values() ? "OK" : "FAIL");
-    printf("The functioning of set_member_of() has been tested ... \t\t%s\n", check_set_member_of() ? "OK" : "FAIL");
-    printf("The functioning of set_is_empty() has been tested ... \t\t%s\n", check_set_is_empty() ? "OK" : "FAIL");
-    printf("The functioning of set_choose() has been tested ... \t\t%s\n", check_set_choose() ? "OK" : "FAIL");
-    printf("The functioning of set_equal() has been tested ... \t\t%s\n", check_set_equal() ? "OK" : "FAIL");
-    printf("The functioning of set_subset() has been tested ... \t\t%s\n", check_set_subset() ? "OK" : "FAIL");
-    printf("The functioning of set_remove() has been tested ... \t\t%s\n", check_set_remove() ? "OK" : "FAIL");
-    printf("The functioning of set_destroy() has been tested ... \t\t%s\n", check_set_destroy() ? "OK" : "FAIL");
-    printf("The functioning of set_size() has been tested ... \t\t%s\n", check_set_size() ? "OK" : "FAIL");
+    // * Test SET_EMPTY() *
+    set *s1 = set_empty();
+    set *s2 = set_empty();
+
+    // * Test SET_SINGLE() *
+    set *s3 = check_set_single();
+
+    // * Test SET_INSERT() *
+    set *s4 = check_set_insert();
+
+    // * Test SET_UNION() *
+    set *s5 = set_empty();
+    set *s6 = set_empty();
+    for (int i = 0; i < 100; i++) {
+        set_insert(i, s5);
+    }
+    for (int i = 50; i < 150; i++) {
+        set_insert(i, s6);
+    }
+    check_set_union(s5, s6);
+
+    // * Test SET_INTERSECTION() *
+    check_set_intersection(s5, s6);
+
+    // * Test SET_DIFFERENCE() *
+    check_set_difference(s5, s6);
+
+    // * Test SET_GET_VALUES() *
+    check_set_get_values(s5);
+
+    // * Test SET_MEMBER_OF() *
+    check_set_member_of(s5);
+
+    // * Test SET_IS_EMPTY() *
+    set *s7 = set_empty();
+    check_set_is_empty(s7);
+
+    // * Test SET_CHOOSE() *
+    check_set_choose(s5);
+
+    // * Test SET_EQUAL() *
+    set *s8 = set_empty();
+    set *s9 = set_empty();
+    for (int i = 0; i < 100; i++) {
+        set_insert(i, s8);
+        set_insert(i, s9);
+    }
+    check_set_equal(s8, s9);
+
+    // * Test SET_SUBSET() *
+    set *s10 = set_empty();
+    for (int i = 50; i < 75; i++) {
+        set_insert(i, s10);
+    }
+    check_set_subset(s10, s5);
+
+    // * Test SET_SIZE() *
+    check_set_size(s5);
+
+    // * Test SET_DESTROY() using valgrind
+    set_destroy(s1);
+    set_destroy(s2);
+    set_destroy(s3);
+    set_destroy(s4);
+    set_destroy(s5);
+    set_destroy(s6);
+    set_destroy(s7);
+    set_destroy(s8);
+    set_destroy(s9);
+    set_destroy(s10);
 
     return 0;
 }
 
-bool check_set_empty(void)
+void check_set_empty(set *s1, set *s2)
 {
-    set *s1 = set_empty();
-    set *s2 = set_empty();
+    bool is_ok = false;
 
-    return (s1->size == 0 && s2->size == 0);
+    if (s1->size == 0 && s2->size == 0) {
+        is_ok = true;
+    }
+
+    printf("The functioning of set_empty() has been tested ... \t\t%s", is_ok ? "OK" : "FAIL");
 }
 
-bool check_set_single(void)
+set *check_set_single(void)
 {
-    /* --- CHECK SET_SINGLE() FUNCTIONALITY --- */
-    set *s3 = set_single(1000);
-    set *s4 = set_single(10);
+    bool is_ok = false;
 
-    if (s3->size == 1 && s4->size == 1) {
-        if ((s3->array[1000 / 8] & (1 << (7 - 1000 % 8))) && (s4->array[10 / 8] & (1 << (7 - 10 % 8)))) {
-            return true;
-        }
-        else {
-            return false;
+    set *s = set_single(1000);
+
+    if (s->size == 1) {
+        if ((s->array[1000 / 8] & (1 << (7 - 1000 % 8)))) {
+            is_ok = true;
         }
     }
-    else {
-        return false;
-    }
+    printf("The functioning of set_single() has been tested ... \t\t%s\n", is_ok ? "OK" : "FAIL");
+
+    return s;
 }
 
-bool check_set_insert(void)
+set *check_set_insert(void)
 {
-    /* --- CHECK SET_INSERT() FUNCTIONALITY --- */
-    set *s5 = set_empty();
+    set *s = set_empty();
+    bool is_ok = true;
 
     for (int i = 0; i < 100; i++) {
-        set_insert(i, s5);
+        set_insert(i, s);
     }
 
-    if (s5->size == 100) {
+    if (s->size == 100) {
         for (int i = 0; i < 100; i++) {
-            if (!(s5->array[i / 8] & (1 << (7 - i % 8)))) {
-                return false;
+            if (!(s->array[i / 8] & (1 << (7 - i % 8)))) {
+                is_ok = false;
             }
         }
-        return true;
     }
     else {
-        return false;
+        is_ok = false;
     }
+    printf("The functioning of set_insert() has been tested ... \t\t%s\n", is_ok ? "OK" : "FAIL");
+
+    return s;
 }
 
-bool check_set_union(void)
+void check_set_union(set *s1, set *s2)
 {
-    /* --- CHECK SET_UNION() FUNCTIONALITY --- */
-    set *s6 = set_empty();
-    set *s7 = set_empty();
+    bool is_ok = true;
 
-    for (int i = 0; i < 100; i++) {
-        set_insert(i, s6);
-    }
-    for (int i = 50; i < 150; i++) {
-        set_insert(i, s7);
-    }
-
-    set *s8 = set_union(s6, s7);
-    if (s8->size == 150) {
+    set *s3 = set_union(s1, s2);
+    if (s3->size == 150) {
         for (int i = 0; i < 150; i++) {
-            if (!(s8->array[i / 8] & (1 << (7 - i % 8)))) {
-                return false;
+            if (!(s3->array[i / 8] & (1 << (7 - i % 8)))) {
+                is_ok = false;
             }
         }
-        return true;
     }
     else {
-        return false;
+        is_ok = false;
     }
+    printf("The functioning of set_union() has been tested ... \t\t%s\n", is_ok ? "OK" : "FAIL");
+
+    set_destroy(s3);
 }
 
-bool check_set_intersection(void)
+void check_set_intersection(set *s1, set *s2)
 {
-    set *s9 = set_empty();
-    set *s10 = set_empty();
+    bool is_ok = true;
 
-    for (int i = 0; i < 100; i++) {
-        set_insert(i, s9);
-    }
-    for (int i = 50; i < 150; i++) {
-        set_insert(i, s10);
-    }
-
-    set *s11 = set_intersection(s9, s10);
-    if (s11->size == 50) {
+    set *s3 = set_intersection(s1, s2);
+    if (s3->size == 50) {
         for (int i = 50; i < 100; i++) {
-            if (!(s11->array[i / 8] & (1 << (7 - i % 8)))) {
-                return false;
+            if (!(s3->array[i / 8] & (1 << (7 - i % 8)))) {
+                is_ok = false;
             }
         }
-        return true;
     }
     else {
-        return false;
+        is_ok = false;
     }
+    printf("The functioning of set_intersection() has been tested ... \t%s\n", is_ok ? "OK" : "FAIL");
+
+    set_destroy(s3);
 }
 
-bool check_set_difference(void)
+void check_set_difference(set *s1, set *s2)
 {
-    set *s12 = set_empty();
-    set *s13 = set_empty();
+    bool is_ok = true;
 
-    for (int i = 0; i < 100; i++) {
-        set_insert(i, s12);
-    }
-    for (int i = 50; i < 150; i++) {
-        set_insert(i, s13);
-    }
-
-    set *s14 = set_difference(s12, s13);
-    if (s14->size == 50) {
+    set *s3 = set_difference(s1, s2);
+    if (s3->size == 50) {
         for (int i = 0; i < 50; i++) {
-            if (!(s14->array[i / 8] & (1 << (7 - i % 8)))) {
-                return false;
+            if (!(s3->array[i / 8] & (1 << (7 - i % 8)))) {
+                is_ok = false;
             }
         }
-        return true;
     }
     else {
-        return false;
+        is_ok = false;
     }
-}
-bool check_set_get_values(void)
-{
-    set *s15 = set_empty();
+    printf("The functioning of set_difference() has been tested ... \t%s\n", is_ok ? "OK" : "FAIL");
 
-    for (int i = 0; i < 100; i++) {
-        set_insert(i, s15);
-    }
-    int *values = set_get_values(s15);
+    set_destroy(s3);
+}
+
+void check_set_get_values(set *s)
+{
+    bool is_ok = true;
+
+    int *values = set_get_values(s);
 
     for (int i = 0; i < 100; i++) {
         if (values[i] != i) {
-            return false;
+            is_ok = false;
+        }
+    }
+    printf("The functioning of set_get_values() has been tested ... \t%s\n", is_ok ? "OK" : "FAIL");
+
+    free(values);
+}
+void check_set_member_of(set *s)
+{
+    bool is_ok = true;
+
+    for (int i = 0; i < 100; i++) {
+        if (!set_member_of(i, s)) {
+            is_ok = false;
+        }
+    }
+    printf("The functioning of set_member_of() has been tested ... \t\t%s\n", is_ok ? "OK" : "FAIL");
+}
+void check_set_is_empty(set *s)
+{
+
+    bool is_ok = (s->size == 0) ? true : false; // ! Maybe redundant !
+    printf("The functioning of set_is_empty() has been tested ... \t\t%s\n", set_is_empty(s) && is_ok ? "OK" : "FAIL");
+}
+void check_set_choose(set *s)
+{
+    int value = set_choose(s);
+    bool is_ok = set_member_of(value, s) && value == 0 ? true : false;
+
+    printf("The functioning of set_choos() has been tested ... \t\t%s\n", is_ok ? "OK" : "FAIL");
+}
+void check_set_equal(set *s1, set *s2)
+{
+    bool is_ok = true;
+
+    if (s1->size != s2->size) {
+        is_ok = false;
+    }
+    else {
+        for (int i = 0; i < s1->capacity; i++) {
+            if (set_member_of(i, s1) != set_member_of(i, s2)) {
+                is_ok = false;
+                break;
+            }
         }
     }
 
-    return true;
+    printf("The functioning of set_equal() has been tested ... \t\t%s\n", is_ok ? "OK" : "FAIL");
 }
-bool check_set_member_of(void)
-{
-    set *s16 = set_empty();
 
+void check_set_subset(set *s1, set *s2)
+{
+    printf("The functioning of set_subset() has been tested ... \t\t%s\n", set_subset(s1, s2) ? "OK" : "FAIL");
+}
+
+void check_set_remove(set *s)
+{
     for (int i = 0; i < 100; i++) {
-        set_insert(i, s16);
+        set_insert(i, s);
     }
-    for (int i = 0; i < 100; i++) {
-        if (!set_member_of(i, s16)) {
-            return false;
-        }
-    }
+    set_remove(50, s);
 
-    return true;
+    bool is_ok = !set_member_of(50, s) ? true : false;
+    printf("The functioning of set_remove() has been tested ... \t\t%s\n", is_ok ? "OK" : "FAIL");
 }
-bool check_set_is_empty(void)
+
+void check_set_size(set *s)
 {
-    set *s17 = set_empty();
-    return set_is_empty(s17);
-}
-bool check_set_choose(void)
-{
-    set *s18 = set_empty();
-
-    for (int i = 0; i < 100; i++) {
-        set_insert(i, s18);
-    }
-    int value = set_choose(s18);
-
-    return set_member_of(value, s18);
-}
-bool check_set_equal(void)
-{
-    set *s19 = set_empty();
-    set *s20 = set_empty();
-
-    for (int i = 0; i < 100; i++) {
-        set_insert(i, s19);
-        set_insert(i, s20);
-    }
-
-    return set_equal(s19, s20);
-}
-bool check_set_subset(void)
-{
-    set *s21 = set_empty();
-    set *s22 = set_empty();
-
-    for (int i = 0; i < 100; i++) {
-        set_insert(i, s21);
-    }
-    for (int i = 50; i < 75; i++) {
-        set_insert(i, s22);
-    }
-
-    return set_subset(s22, s21);
-}
-bool check_set_remove(void)
-{
-    set *s23 = set_empty();
-    for (int i = 0; i < 100; i++) {
-        set_insert(i, s23);
-    }
-    set_remove(50, s23);
-
-    return !set_member_of(50, s23);
-}
-bool check_set_destroy(void)
-{
-    set *s24 = set_empty();
-    set_destroy(s24);
-    return true; //! how to check if the set is destroyed?
-}
-bool check_set_size(void)
-{
-    set *s25 = set_empty();
-    for (int i = 0; i < 100; i++) {
-        set_insert(i, s25);
-    }
-
-    return set_size(s25) == 100;
+    bool is_ok = (s->size == 100) ? true : false;
+    printf("The functioning of set_size() has been tested ... \t\t%s\n", (set_size(s) == 100 && is_ok) ? "OK" : "FAIL");
 }
